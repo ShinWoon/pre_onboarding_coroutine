d214de7605dd9c857d4bb15029ab6b84585b4a21
ShinWoon <brandy.hwang@gmail.com>
2024-02-29 23:01:40 +0900

    ✨Exception, try-catch, cancel

--- CodelabsCoroutine/src/main/kotlin/Main.kt
+++ CodelabsCoroutine/src/main/kotlin/Main.kt
@@ -1,21 +1,32 @@
 import kotlinx.coroutines.*
+import kotlin.AssertionError
 import kotlin.system.measureTimeMillis
 
 fun main() {
     val time = measureTimeMillis {
         runBlocking {
             println("Weather forecast")
-            val forecast: Deferred<String> = async {
-                getForecast()
-            }
-            val temperature: Deferred<String> = async {
-                getTemperature()
-            }
-            println("${forecast.await()} ${temperature.await()}")
+//            try {
+//                println(getWeatherReport())
+//            } catch (e: AssertionError) {
+//                println("Caught exception in runBlocking(): $e")
+//                println("Report unavailable at this time")
+//            }
+            println(getWeatherReport())
             println("Have a good day!")
         }
     }
     println("Execution time: ${time / 1000.0} seconds")
+}
+
+suspend fun getWeatherReport() = coroutineScope {
+    val forecast = async { getForecast() }
+    val temperature = async {
+        getTemperature()
+    }
+    delay(200)
+    temperature.cancel()
+    "${forecast.await()} ${temperature.await()}"
 }
 
 suspend fun printForecast() {   // 정지 함수
@@ -27,6 +38,7 @@ suspend fun printTemperature() {
     delay(1000)
     println("30\u00b0C")
 }
+
 suspend fun getForecast(): String {
     delay(1000)
     return "Sunny"
@@ -34,5 +46,6 @@ suspend fun getForecast(): String {
 
 suspend fun getTemperature(): String {
     delay(1000)
+//    throw AssertionError("Temperature is invalid")
     return "30\u00b0C"
 }
