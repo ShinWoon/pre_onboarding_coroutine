ef2efe6fede231b28d9a5a6c1de2d9ea46560121
ShinWoon <brandy.hwang@gmail.com>
2024-02-29 22:42:41 +0900

    ✨Asynchronous code - async

--- CodelabsCoroutine/src/main/kotlin/Main.kt
+++ CodelabsCoroutine/src/main/kotlin/Main.kt
@@ -5,12 +5,13 @@ fun main() {
     val time = measureTimeMillis {
         runBlocking {
             println("Weather forecast")
-            launch {
-                printForecast()
+            val forecast: Deferred<String> = async {
+                getForecast()
             }
-            launch {
-                printTemperature()
+            val temperature: Deferred<String> = async {
+                getTemperature()
             }
+            println("${forecast.await()} ${temperature.await()}")
             println("Have a good day!")
         }
     }
@@ -25,4 +26,13 @@ suspend fun printForecast() {   // 정지 함수
 suspend fun printTemperature() {
     delay(1000)
     println("30\u00b0C")
+}
+suspend fun getForecast(): String {
+    delay(1000)
+    return "Sunny"
+}
+
+suspend fun getTemperature(): String {
+    delay(1000)
+    return "30\u00b0C"
 }
